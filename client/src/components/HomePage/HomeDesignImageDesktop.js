4c420b051dbbcca595e06b605661703d9372d6f8
Chris Tennant <exceritys@gmail.com>
2021-07-08 04:03:01 -0400

    Updated Design Images Link

--- client/src/components/HomePage/HomeDesignImageDesktop.js
+++ client/src/components/HomePage/HomeDesignImageDesktop.js
@@ -20,7 +20,7 @@ function HomeDesignImageDesktop({title}) {
     
        
             <div className='' id='App'>
-              <Link to='AppDesign#top'>
+              <Link to='/AppDesign#top'>
               <div className='Desktop-image-desc-container'>
             <h1 className='Desktop-design-title'>App Design</h1>
             <h3 className='Desktop-design-h3 Desktop-app-h3'>View Projects<svg className='Design__Image__Arrow' width="7" height="10" xmlns="http://www.w3.org/2000/svg"><path d="M1 1l4 4-4 4" stroke="#E7816B" stroke-width="2" fill="none" fill-rule="evenodd"/>
@@ -31,7 +31,7 @@ function HomeDesignImageDesktop({title}) {
             </div>
 
             <div className='Home__Desktop  Desktop__Graphic__Design' id='Graphic'>
-              <Link to='GraphicDesign#top'>
+              <Link to='/GraphicDesign#top'>
             <div className='Desktop-image-desc-container'>
             <h1 className='Desktop-design-title'>graphic design</h1>
             <h3 className='Desktop-design-h3 Desktop-graphic-h3'>View Projects<svg className='Design__Image__Arrow' width="7" height="10" xmlns="http://www.w3.org/2000/svg"><path d="M1 1l4 4-4 4" stroke="#E7816B" stroke-width="2" fill="none" fill-rule="evenodd"/>
